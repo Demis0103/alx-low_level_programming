e605ffdac19bf333ac50f9697ec377afa883c6bc
Demis0103 <demisabebaw@gmail.com>
2022-09-10 06:40:37 -0700

    com

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+nclude <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
@@ -10,12 +10,16 @@
  */
 int main(void)
 {
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z' ; low++)
+{
+	if (low != e && low ! = q)
 	putchar(low);
-	for (low = 'A'; low <= 'Z' ; low++)
-	putchar(low);
+}
 	printf("\n");
 
 	return (0);
