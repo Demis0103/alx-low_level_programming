e3c2c574f36f4f22e08164ddf6c41a55a0577b97
Demis0103 <demisabebaw@gmail.com>
2022-09-28 07:24:47 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-* is_prime_number - Checks if a number is divi* * *
-* Return: 1 if n is prime. 0 otherwise
+* is_prime_number - Checks if a number is div
+*@n: number
+* * Return: 1 if n is prime. 0 otherwise
 */
 int is_prime_number(int n)
 {
@@ -11,8 +12,7 @@ return (0);
 return (is_prime(n, start));
 }
 /**
-* is_prime - Checks if a number is divisible. * @n: The number to be checked.
-* * *
+* is_prime - Checks if a number is divisible. * @start: The number to be che* * *
 * Return: 1 if n is prime. 0 otherwise
 */
 int is_prime(int n, int start)
