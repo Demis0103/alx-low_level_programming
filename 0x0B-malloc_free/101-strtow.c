8a44e724e8af1190c3224e2dcc3a25bb9df356cd
Demis0103 <demisabebaw@gmail.com>
2022-10-04 11:01:05 -0700

    com

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,51 +1,31 @@
+#include <stdlib.h>
 #include "main.h"
 /**
- * len - retuns length
- * @str: string to be coiunted
+ * count_word - retuns length
+ * @s: string to be coiunted
  *
  * Return: length of the string
  */
 
-int len(char *str)
+int count_word(char *s)
 {
-	int len = 0;
+	int flag, c, w;
 
-	if (str != NULL)
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
 	{
-	while (str[len])
-	len++;
-	}
-	return (len);
-}
-
-/**
- * num_words - counts the number
- * @str: string to be used
- *
- * Return: number of words
- */
-
-int num_words(char *str)
-{
-	int i = 0, words = 0;
-
-	while (i <= len(str))
-	{
-		if ((str[i] != ' ') && (str[i] != '\0'))
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
-		i++;
+			flag = 1;
+			w++;
+		}
 	}
-	else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-	{
-	words += 1;
-	i++;
-	}
-	else
-	{
-	i++;
-	}
-	return (words);
-	}
+	return (w);
+}
 
 /**
  * strtow - splits a string into words
@@ -56,45 +36,40 @@ int num_words(char *str)
 
 char **strtow(char *str)
 {
-	char **split;
+	char **matrix, *tmp;
 
-	int i, j = 0, temp = 0, size = 0, words = num_words(str);
+	int i, k = 0, len = 0, c = 0, words, start, end;
 
+	while (*(str + len))
+		len++;
+	words = count_word(str);
 	if (words == 0)
 		return (NULL);
-	split = (char **)malloc(sizeof(char *) * (words + 1));
-	if (split != NULL)
-	{
-		for (i = 0; i <= len(str) && words; i++)
-		{
-			if ((str[i] != ' ') && (str[i] != '\0'))
-				size++;
-	else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-	{
-		split[j] = (char *)malloc(sizeof(char) * size + 1);
-		if (split[j] != NULL)
-		{
-			while (temp < size)
-			{
-				split[j][temp] = str[(i - size) + temp];
-				temp++;
-	}
-			split[j][temp] = '\0';
-			size = temp = 0;
-			j++;
-		}
-		else
-		{
-			while (j-- >= 0)
-				free(split[j]);
-			free(split);
-			return (NULL);
-		}
-	}
-		}
-		split[words] = NULL;
-		return (split);
-	}
-	else
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
+
+		for (i = 0; i <= len; i++)
+		{
+			if (str[i] == ' ' || str[i] == '\0')
+			{
+				if (c)
+				{
+					ende = i;
+		tmp = (char *) malloc(sizeof(char) * (c + 1));
+		if (tmp == NULL)
+			return (NULL);
+		while (start < end)
+			*tmp++ = str[start++];
+		*tmp = '\0';
+		matrix[k] = tmp - c;
+		k++;
+		c = 0;
+				}
+			}
+			else if (c++ == 0)
+				start = i;
+		}
+		matrix[k] = NULL;
+		return (matrix);
 }
