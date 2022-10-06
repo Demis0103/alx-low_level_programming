e583b8e6a89bbd13d8ba9bd4dc5f80ed60a8d336
Demis0103 <demisabebaw@gmail.com>
2022-10-06 11:02:20 -0700

    com

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - alloactes nenory
+ * #nmemb: bummber of elements
+ * @size: size in bytes
+ *
+ * Return: void pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NUll)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}
