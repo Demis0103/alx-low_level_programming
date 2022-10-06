1a322025aa0e9ed8495843bbc61504fdff3ff676
Demis0103 <demisabebaw@gmail.com>
2022-10-06 12:13:30 -0700

    com

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
+/**
  * array_range - creates an array of interges.
  * @min: first integre
  * @max: last integer
@@ -11,7 +11,7 @@
 
 int *array_range(int min, int max)
 {
-	int *arr, i = 0,t = min;
+	int *arr, i = 0, t = min;
 
 	if (min > max)
 	return (0);
