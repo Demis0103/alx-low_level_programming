b20f7483eb49e052deb16fe03914ff76877c7bf8
Demis0103 <demisabebaw@gmail.com>
2022-10-10 05:32:34 -0700

    com

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ return (len);
 }
 
 /**
- * _strcopy - Copies a string pointed to by src, including the terminating null byte, to a buffer pointed to by dest.
+ * _strcopy - Copies a string pointed to by src, ito a buffer pointed to by dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
  *
